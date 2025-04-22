526d8171c63a68b76a1e4db49430478dad3fb0b8
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-04-22 12:02:55 -0300

    atualizado geração de arquivos para só pegar o projeto e a quantidade de commits totais

--- query.py
+++ query.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 
 url_metadados = "https://drive.google.com/uc?export=download&id=19cJ7avNtsziaYkrrYuW7FeFdvgrxoNLc"
 
-cred = credentials.Certificate("tcc-ufape-jose-daniel-firebase-adminsdk-fbsvc-54b983ae3d.json")
+cred = credentials.Certificate("C:\\Users\\jose.filho.ext\\Desktop\\tcc-ufape-jose-daniel-firebase-adminsdk-fbsvc-ae66b7718d.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -27,30 +27,23 @@ for i, linha in enumerate(dados_brutos):
 
 metadados = pd.DataFrame(dados_processados)
 
-if 'repo_url' in metadados.columns:
-    contagem_commits = metadados['repo_url'].value_counts()
-    
+if 'project' in metadados.columns:
+    contagem_commits = metadados['project'].value_counts()
+
     contagem_commits_df = contagem_commits.reset_index()
-    contagem_commits_df.columns = ['URL do Repositorio', 'Numero de Commits']
-    
-    contagem_commits_df['Projeto'] = contagem_commits_df['URL do Repositorio'].apply(lambda x: x.split('/')[-1])
-    
-    contagem_commits_df = contagem_commits_df[['Projeto', 'URL do Repositorio', 'Numero de Commits']]
+    contagem_commits_df.columns = ['Projeto', 'Numero de Commits']
 
     contagem_commits_df.to_csv('quantidade_commits.csv', index=False, sep=',', encoding='utf-8')
-    
+
     for _, linha in contagem_commits_df.iterrows():
         projeto_ref = db.collection('commits-vunerabilidades-diversevul').document() 
         projeto_ref.set({
             'Projeto': linha['Projeto'],
-            'URL do Repositorio': linha['URL do Repositorio'],
             'Numero de Commits': linha['Numero de Commits']
         })
-    
+
     print("Dados enviados para o Firestore com sucesso!")
 
-    contagem_commits_df.to_csv('quantidade_commits.csv', index=False, sep=',', encoding='utf-8')
-    
     with open('quantidade_commits.txt', 'w', encoding='utf-8') as f:
         for _, linha in contagem_commits_df.iterrows():
-            f.write(f"Projeto: {linha['Projeto']} - URL: {linha['URL do Repositorio']} - Quantidade de Commits: {linha['Numero de Commits']}\n")
+            f.write(f"Projeto: {linha['Projeto']} - Quantidade de Commits: {linha['Numero de Commits']}\n")
